cb2df3757008173a68147df22a3e17334919ff17
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-23 00:42:03 +0530

    removed console.log statement

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,7 +12,6 @@ router.get('/callback', (req, res) => {
   const sessionsStore = req.app.locals.sessionsStore;
   const code = req.query.code;
   getGithubAccessToken(code).then(token => {
-    console.log(token);
     res.cookie('id', sessionsStore.createNewSession(token));
     req.redisClient.set('sessionsStore', sessionsStore.toJSON());
     res.redirect(process.env.LOGIN_REDIRECT);
