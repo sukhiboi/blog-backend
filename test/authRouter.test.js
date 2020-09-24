dfbe5dc53dc5dd425ecd14b912c3e0f2567ebeb8
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-24 19:11:14 +0530

    refactored tests

--- test/authRouter.test.js
+++ test/authRouter.test.js
@@ -4,7 +4,27 @@ const app = require('./../app');
 const axios = require('axios');
 const { assert } = require('chai');
 
-describe.only('Auth Router()', () => {
+describe('Auth Router()', () => {
+  let axiosPostStub;
+  let axiosGetStub;
+
+  beforeEach(() => {
+    axiosPostStub = sinon
+      .stub(axios, 'post')
+      .resolves({ data: { access_token: 2468 } });
+
+    axiosGetStub = sinon.stub(axios, 'get').resolves({
+      data: { name: 'sukhdev', bio: 'hello', avatar_url: 'url' },
+    });
+  });
+
+  afterEach(done => {
+    axiosPostStub.restore();
+    axiosGetStub.restore();
+    sinon.restore();
+    done();
+  });
+
   describe('GET /api/auth/login', () => {
     it('should redirect to the github authorization server', done => {
       request(app)
@@ -15,20 +35,7 @@ describe.only('Auth Router()', () => {
     });
   });
 
-  describe.only('GET /api/auth/callback', () => {
-    const axiosPostStub = sinon
-      .stub(axios, 'post')
-      .resolves({ data: { access_token: 2468 } });
-
-    const axiosGetStub = sinon
-      .stub(axios, 'get')
-      .resolves({ data: { name: 'sukhdev', bio: 'hello', avatar_url: 'url' } });
-
-    after(() => {
-      axiosPostStub.restore();
-      axiosGetStub.restore();
-    });
-
+  describe('GET /api/auth/callback', () => {
     it('should should redirect to the app after saving user details', done => {
       request(app)
         .get('/api/auth/callback?code=1234')
