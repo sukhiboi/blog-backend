6a85addbbbfd390f1e46a10b7314782a41ae6968
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-25 12:06:05 +0530

    saving user when it's a new user only

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,16 +12,22 @@ router.get('/login', authorizeUser);
 router.get('/callback', (req, res) => {
   const sessionsStore = req.app.locals.sessionsStore;
   const usersStore = req.app.locals.usersStore;
+  const client = req.redisClient;
   const code = req.query.code;
   getGithubAccessToken(code)
     .then(getUserDetailsByAccessToken)
     .then(({ data, accessToken }) => {
       const user = { name: data.name, imgURL: data.avatar_url, bio: data.bio };
       res.cookie('id', sessionsStore.createNewSession({ accessToken, user }));
-      usersStore.addNewUser(user);
-      req.redisClient.set('sessionsStore', sessionsStore.toJSON());
-      req.redisClient.set('usersStore', usersStore.toJSON());
-      res.redirect(process.env.LOGIN_REDIRECT);
+      client.set('sessionsStore', sessionsStore.toJSON(), (err, res) => {
+        if (usersStore.getUser(user.name)) {
+          return res.redirect(process.env.LOGIN_REDIRECT);
+        }
+        usersStore.addNewUser(user);
+        client.set('usersStore', usersStore.toJSON(), (err, res) => {
+          res.redirect(process.env.LOGIN_REDIRECT);
+        });
+      });
     });
 });
 
