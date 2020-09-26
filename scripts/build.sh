0b1cdb2e384e1957d84e9d3bda64f36dfef747cf
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-26 16:31:31 +0530

    updated build script to build the whole project

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,30 @@
+#cloning frontend
 git clone https://github.com/sukhiboi/blog-frontend.git;
+
+#creating react build
 cd blog-frontend;
 npm install;
 npm test;
 npm run build;
+
+#cloning build to root
 mv build ../build;
+
+#deleting frontend
 cd ..;
 rm -rf blog-frontend;
+
+#cloning backend
+git clone https://github.com/sukhiboi/blog-backend.git;
+
+cd blog-backend;
+npm install;
+npm test;
+rm -rf node_modules;
+
+#cloning backend to root
+cp -R ./* to ..;
+
+#deleting backend
+cd ..;
+rm -rf blog-backend;
