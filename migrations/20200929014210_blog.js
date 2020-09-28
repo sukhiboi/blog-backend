ac2e0fad7e0b982e351948741058e0a0a4483fbb
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-29 01:42:34 +0530

    updated postgres migration

--- migrations/20200929014210_blog.js
+++ migrations/20200929014210_blog.js
@@ -2,16 +2,16 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('posts', table => {
       table.increments('id');
-      table.string('title').notNullable();
-      table.string('content').notNullable();
+      table.text('title').notNullable();
+      table.text('content').notNullable();
       table.timestamp('published_on').defaultTo(knex.fn.now());
       table.integer('user_id').notNullable();
     })
     .createTable('users', table => {
       table.increments('user_id');
       table.string('user_name').notNullable();
-      table.string('img_url');
-      table.string('bio');
+      table.text('img_url');
+      table.text('bio');
     });
 };
 
