9227f59e977e3b05bc6b42c6d502494dac48c267
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-01 04:08:20 +0530

    updated database methods to fetch less info about posts

--- src/database.js
+++ src/database.js
@@ -34,7 +34,7 @@ class Database {
     return new Promise((resolve, reject) => {
       this.client('posts')
         .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('title', 'published_on', 'users.user_id', 'id', 'user_name')
+        .select('title', 'published_on', 'users.user_id', 'user_name', 'id')
         .where('users.user_id', '=', user_id)
         .then(resolve)
         .catch(reject);
