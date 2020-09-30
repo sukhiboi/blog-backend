c422f272d497785d414bb3e0f9a7d7b7a23326b7
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-01 04:07:42 +0530

    made Button a styled component

--- src/database.js
+++ src/database.js
@@ -34,7 +34,7 @@ class Database {
     return new Promise((resolve, reject) => {
       this.client('posts')
         .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('*')
+        .select('title', 'published_on', 'users.user_id', 'id', 'user_name')
         .where('users.user_id', '=', user_id)
         .then(resolve)
         .catch(reject);
@@ -45,7 +45,7 @@ class Database {
     return new Promise((resolve, reject) => {
       this.client('posts')
         .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('*')
+        .select('title', 'published_on', 'users.user_id', 'id', 'user_name')
         .orderBy('published_on', 'desc')
         .then(resolve)
         .catch(reject);
