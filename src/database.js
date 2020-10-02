3ab28d77cacaa301cf409874f3653e07901195c1
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-10-03 03:27:43 +0530

    added test for database and refatored database.js

--- src/database.js
+++ src/database.js
@@ -4,72 +4,41 @@ class Database {
   }
 
   savePost(post) {
-    return new Promise((resolve, reject) => {
-      this.client('posts').insert(post).then(resolve).catch(reject);
-    });
+    return this.client('posts').insert(post);
   }
 
   deletePost(id) {
-    return new Promise((resolve, reject) => {
-      this.client('posts')
-        .where('id', '=', id)
-        .del()
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('posts').where('id', '=', id).del();
   }
 
   getPost(id) {
-    return new Promise((resolve, reject) => {
-      this.client('posts')
-        .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('*')
-        .where('id', '=', id)
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('posts')
+      .join('users', 'posts.user_id', '=', 'users.user_id')
+      .select('*')
+      .where('id', '=', id);
   }
 
   getUserPosts(user_id) {
-    return new Promise((resolve, reject) => {
-      this.client('posts')
-        .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('title', 'published_on', 'users.user_id', 'user_name', 'id')
-        .orderBy('published_on', 'desc')
-        .where('users.user_id', '=', user_id)
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('posts')
+      .join('users', 'posts.user_id', '=', 'users.user_id')
+      .select('title', 'published_on', 'users.user_id', 'user_name', 'id')
+      .orderBy('published_on', 'desc')
+      .where('users.user_id', '=', user_id);
   }
 
   getAllPosts() {
-    return new Promise((resolve, reject) => {
-      this.client('posts')
-        .join('users', 'posts.user_id', '=', 'users.user_id')
-        .select('title', 'published_on', 'users.user_id', 'id', 'user_name')
-        .orderBy('published_on', 'desc')
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('posts')
+      .join('users', 'posts.user_id', '=', 'users.user_id')
+      .select('title', 'published_on', 'users.user_id', 'user_name', 'id')
+      .orderBy('published_on', 'desc');
   }
 
   saveUser(user) {
-    return new Promise((resolve, reject) => {
-      this.client('users')
-        .insert(user, ['user_id'])
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('users').insert(user, ['user_id']);
   }
 
   getUser(user_name) {
-    return new Promise((resolve, reject) => {
-      this.client('users')
-        .select('*')
-        .where('user_name', '=', user_name)
-        .then(resolve)
-        .catch(reject);
-    });
+    return this.client('users').select('*').where('user_name', '=', user_name);
   }
 }
 
---
+++ test/database.test.js
@@ -0,0 +1,308 @@
+const { assert } = require('chai');
+const { fake } = require('sinon');
+const Database = require('./../src/database');
+
+describe('Database', () => {
+  const error = new Error('postgres connection failed');
+
+  describe('savePost', () => {
+    it('should resolve if post got saved', async () => {
+      const fakeInsert = fake.resolves('OK');
+      const client = fake.returns({ insert: fakeInsert });
+      const post = { postId: 1 };
+      const db = new Database(client);
+      const res = await db.savePost(post);
+      assert.strictEqual(res, 'OK');
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('posts'));
+      assert.strictEqual(fakeInsert.callCount, 1);
+      assert.ok(fakeInsert.calledWith(post));
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeInsert = fake.rejects(error);
+      const client = fake.returns({ insert: fakeInsert });
+      const post = { postId: 1 };
+      const db = new Database(client);
+      try {
+        await db.savePost(post);
+      } catch (e) {
+        assert.strictEqual(e, error);
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('posts'));
+        assert.strictEqual(fakeInsert.callCount, 1);
+        assert.ok(fakeInsert.calledWith(post));
+      }
+    });
+  });
+
+  describe('deletePost', () => {
+    it('should resolve after deleting post', async () => {
+      const fakeDel = fake.resolves('OK');
+      const fakeWhere = fake.returns({ del: fakeDel });
+      const client = fake.returns({ where: fakeWhere });
+      const db = new Database(client);
+      const res = await db.deletePost(1);
+      assert.strictEqual(res, 'OK');
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('posts'));
+      assert.strictEqual(fakeWhere.callCount, 1);
+      assert.ok(fakeWhere.calledWith('id', '=', 1));
+      assert.strictEqual(fakeDel.callCount, 1);
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeDel = fake.rejects(error);
+      const fakeWhere = fake.returns({ del: fakeDel });
+      const client = fake.returns({ where: fakeWhere });
+      const db = new Database(client);
+      try {
+        await db.deletePost(1);
+      } catch (e) {
+        assert.strictEqual(e, error);
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('posts'));
+        assert.strictEqual(fakeWhere.callCount, 1);
+        assert.ok(fakeWhere.calledWith('id', '=', 1));
+        assert.strictEqual(fakeDel.callCount, 1);
+      }
+    });
+  });
+
+  describe('getPost', () => {
+    it('should resolve post', async () => {
+      const fakeWhere = fake.resolves('OK');
+      const fakeSelect = fake.returns({ where: fakeWhere });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      const res = await db.getPost(1);
+      assert.strictEqual(res, 'OK');
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('posts'));
+      assert.strictEqual(fakeJoin.callCount, 1);
+      assert.ok(
+        fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+      );
+      assert.strictEqual(fakeSelect.callCount, 1);
+      assert.ok(fakeSelect.calledWith('*'));
+      assert.strictEqual(fakeWhere.callCount, 1);
+      assert.ok(fakeWhere.calledWith('id', '=', 1));
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeWhere = fake.rejects(error);
+      const fakeSelect = fake.returns({ where: fakeWhere });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      try {
+        await db.getPost(1);
+      } catch (e) {
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('posts'));
+        assert.strictEqual(fakeJoin.callCount, 1);
+        assert.ok(
+          fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+        );
+        assert.strictEqual(fakeSelect.callCount, 1);
+        assert.ok(fakeSelect.calledWith('*'));
+        assert.strictEqual(fakeWhere.callCount, 1);
+        assert.ok(fakeWhere.calledWith('id', '=', 1));
+      }
+    });
+  });
+
+  describe('getUserPost', () => {
+    it('should resolve user posts', async () => {
+      const fakeWhere = fake.resolves('OK');
+      const fakeOrderBy = fake.returns({ where: fakeWhere });
+      const fakeSelect = fake.returns({ orderBy: fakeOrderBy });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      const res = await db.getUserPosts(1);
+      assert.strictEqual(res, 'OK');
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('posts'));
+      assert.strictEqual(fakeJoin.callCount, 1);
+      assert.ok(
+        fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+      );
+      assert.strictEqual(fakeSelect.callCount, 1);
+      assert.ok(
+        fakeSelect.calledWith(
+          'title',
+          'published_on',
+          'users.user_id',
+          'user_name',
+          'id'
+        )
+      );
+      assert.strictEqual(fakeOrderBy.callCount, 1);
+      assert.ok(fakeOrderBy.calledWith('published_on', 'desc'));
+      assert.strictEqual(fakeWhere.callCount, 1);
+      assert.ok(fakeWhere.calledWith('users.user_id', '=', 1));
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeWhere = fake.resolves('OK');
+      const fakeOrderBy = fake.returns({ where: fakeWhere });
+      const fakeSelect = fake.returns({ orderBy: fakeOrderBy });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      try {
+        await db.getUserPosts(1);
+      } catch (e) {
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('posts'));
+        assert.strictEqual(fakeJoin.callCount, 1);
+        assert.ok(
+          fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+        );
+        assert.strictEqual(fakeSelect.callCount, 1);
+        assert.ok(
+          fakeSelect.calledWith(
+            'title',
+            'published_on',
+            'users.user_id',
+            'user_name',
+            'id'
+          )
+        );
+        assert.strictEqual(fakeOrderBy.callCount, 1);
+        assert.ok(fakeOrderBy.calledWith('published_on', 'desc'));
+        assert.strictEqual(fakeWhere.callCount, 1);
+        assert.ok(fakeWhere.calledWith('users.user_id', '=', 1));
+      }
+    });
+  });
+
+  describe('getAllPosts', () => {
+    it('should resolve all posts', async () => {
+      const fakeOrderBy = fake.resolves('OK');
+      const fakeSelect = fake.returns({ orderBy: fakeOrderBy });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      const res = await db.getAllPosts(1);
+      assert.strictEqual(res, 'OK');
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('posts'));
+      assert.strictEqual(fakeJoin.callCount, 1);
+      assert.ok(
+        fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+      );
+      assert.strictEqual(fakeSelect.callCount, 1);
+      assert.ok(
+        fakeSelect.calledWith(
+          'title',
+          'published_on',
+          'users.user_id',
+          'user_name',
+          'id'
+        )
+      );
+      assert.strictEqual(fakeOrderBy.callCount, 1);
+      assert.ok(fakeOrderBy.calledWith('published_on', 'desc'));
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeOrderBy = fake.resolves('OK');
+      const fakeSelect = fake.returns({ orderBy: fakeOrderBy });
+      const fakeJoin = fake.returns({ select: fakeSelect });
+      const client = fake.returns({ join: fakeJoin });
+      const db = new Database(client);
+      try {
+        await db.getAllPosts(1);
+      } catch (e) {
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('posts'));
+        assert.strictEqual(fakeJoin.callCount, 1);
+        assert.ok(
+          fakeJoin.calledWith('users', 'posts.user_id', '=', 'users.user_id')
+        );
+        assert.strictEqual(fakeSelect.callCount, 1);
+        assert.ok(
+          fakeSelect.calledWith(
+            'title',
+            'published_on',
+            'users.user_id',
+            'user_name',
+            'id'
+          )
+        );
+        assert.strictEqual(fakeOrderBy.callCount, 1);
+        assert.ok(fakeOrderBy.calledWith('published_on', 'desc'));
+      }
+    });
+  });
+
+  describe('saveUser', () => {
+    it('should resolve after saving user', async () => {
+      const user = { user_name: 'sukhi' };
+      const fakeInsert = fake.resolves(1);
+      const client = fake.returns({ insert: fakeInsert });
+      const db = new Database(client);
+      const res = await db.saveUser(user);
+      assert.strictEqual(res, 1);
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('users'));
+      assert.strictEqual(fakeInsert.callCount, 1);
+      assert.ok(fakeInsert.calledWith(user, ['user_id']));
+    });
+
+    it('should reject if error occurs', async () => {
+      const user = { user_name: 'sukhi' };
+      const fakeInsert = fake.rejects(error);
+      const client = fake.returns({ insert: fakeInsert });
+      const db = new Database(client);
+      try {
+        await db.saveUser(user);
+      } catch (e) {
+        assert.strictEqual(e, error);
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('users'));
+        assert.strictEqual(fakeInsert.callCount, 1);
+        assert.ok(fakeInsert.calledWith(user, ['user_id']));
+      }
+    });
+  });
+
+  describe('getUser', () => {
+    it('should resolve with user', async () => {
+      const user = { user_name: 'sukhi' };
+      const fakeWhere = fake.resolves(user);
+      const fakeSelect = fake.returns({ where: fakeWhere });
+      const client = fake.returns({ select: fakeSelect });
+      const db = new Database(client);
+      const res = await db.getUser('sukhi');
+      assert.strictEqual(res, user);
+      assert.strictEqual(client.callCount, 1);
+      assert.ok(client.calledWith('users'));
+      assert.strictEqual(fakeSelect.callCount, 1);
+      assert.ok(fakeSelect.calledWith('*'));
+      assert.strictEqual(fakeWhere.callCount, 1);
+      assert.ok(fakeWhere.calledWith('user_name', '=', 'sukhi'));
+    });
+
+    it('should reject if error occurs', async () => {
+      const fakeWhere = fake.rejects(error);
+      const fakeSelect = fake.returns({ where: fakeWhere });
+      const client = fake.returns({ select: fakeSelect });
+      const db = new Database(client);
+      try {
+        await db.getUser('sukhi');
+      } catch (e) {
+        assert.strictEqual(e, error);
+        assert.strictEqual(client.callCount, 1);
+        assert.ok(client.calledWith('users'));
+        assert.strictEqual(fakeSelect.callCount, 1);
+        assert.ok(fakeSelect.calledWith('*'));
+        assert.strictEqual(fakeWhere.callCount, 1);
+        assert.ok(fakeWhere.calledWith('user_name', '=', 'sukhi'));
+      }
+    });
+  });
+});
